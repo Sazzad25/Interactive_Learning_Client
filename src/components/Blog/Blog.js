ebdd2cc9e09c99b734e6f6586e88af0e59c4c12c
Sazzad Hosen <mdsazzadhosentamim95@gmail.com>
2022-10-25 01:24:03 +0600

    added blog part

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,7 +3,26 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h2>This is Blog</h2>
+            <div>
+                <h2>Q.1:  what is cors?</h2>
+                <p>CORS (Cross-Origin Resource Sharing) is a system, consisting of transmitting HTTP headers, that determines whether browsers block frontend JavaScript code from accessing responses for cross-origin requests. The same-origin security policy forbids cross-origin access to resources. In ReactJS, Cross-Origin Resource Sharing (CORS) refers to the method that allows you to make requests to the server deployed at a different domain. As a reference, if the frontend and backend are at two different domains, we need CORS there.</p>
+            </div>
+            <div>
+                <h2>Q.2: Why are you using firebase? What other options do you have to implement authentication?</h2>
+                <p>Firebase provides detailed documentation and cross-platform SDKs to help you build and ship apps on Android, iOS, the web, C++, and Unity. It allows real-time database connection, which means multiple users can see the changes in the data when the data gets created or edited. Data transmission is handled with web sockets so you don't have to send requests to get new data, you only need to subscribe once.
+
+                 <div className='mt-4'> Auth0, MongoDB, Passport, Okta, and Firebase are the most popular alternatives and competitors to Firebase Authentication. </div>
+                </p>
+            </div>
+            <div>
+                <h2>Q.3: How does the private route work?</h2>
+                <p>The react private route component renders child components ( children ) if the user is logged in. If not logged in the user is redirected to the /login page with the return url passed in the location state property. The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).</p>
+            </div>
+            <div>
+                <h2>Q.4: What is Node? How does Node work?</h2>
+                <div>A node is a basic unit of a data structure, such as a linked list or tree data structure. Nodes contain data and also may link to other nodes. Links between nodes are often implemented by pointers.</div>
+                <div className='mt-4'>It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.</div>
+            </div>
         </div>
     );
 };
